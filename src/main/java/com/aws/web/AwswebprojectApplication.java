c26ec56f3f1613dc8a9a8632c61b8e88b84df306
Amod Lal <cloud4amod@gmail.com>
2024-06-07 20:37:31 +0530

    First commit

---
+++ src/main/java/com/aws/web/AwswebprojectApplication.java
@@ -0,0 +1,13 @@
+package com.aws.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwswebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwswebprojectApplication.class, args);
+	}
+
+}
